558f16d568ab2a23b56067e47e720ebc259e16e8
Ray-Shakex <bshakex@gmail.com>
2022-01-24 18:22:30 +0200

    utilities.scss

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 const toggle = document.getElementById("toggle");
+const main = document.getElementById("main");
 const aside = document.getElementById("aside");
-const links = document.getElementById("toggle");
+const links = document.querySelectorAll('#aside ul li a');
+console.log(links);
 
 toggle.addEventListener("click",  () => {
     aside.classList.toggle("active");
+});
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        aside.classList.remove("active");
+    });
+});
+
+main.addEventListener("click", () => {
+    aside.classList.remove("active");
 });
