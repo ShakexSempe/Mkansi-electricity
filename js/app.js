075c782001e084cadd4c83ff27f8e7011a39736b
Ray-Shakex <bshakex@gmail.com>
2022-01-25 11:51:28 +0200

    *

--- js/app.js
+++ js/app.js
@@ -7,16 +7,19 @@ console.log(links);
 toggle.addEventListener("click",  () => {
     aside.classList.toggle("active");
     toggle.classList.toggle("active");
+    main.classList.toggle("active");
 });
 
 links.forEach(link => {
     link.addEventListener("click", () => {
         aside.classList.remove("active");
-        toggle.classList.toggle("active");
+        toggle.classList.remove("active");
+        main.classList.remove("active");
     });
 });
 
 main.addEventListener("click", () => {
     aside.classList.remove("active");
     toggle.classList.remove("active");
+    main.classList.remove("active");
 });
