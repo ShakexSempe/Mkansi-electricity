eb101ca379702dfe4aa00209edd126fb567a7674
Ray-Shakex <bshakex@gmail.com>
2022-02-06 16:22:16 +0200

    desktop nav links hover state

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 const toggle = document.getElementById("toggle");
+const header = document.getElementById("header");
 const main = document.getElementById("main");
 const aside = document.getElementById("aside");
 const links = document.querySelectorAll('#aside ul li a');
 console.log(links);
 
+document.addEventListener("scroll", () => {
+    const scroll_position = window.scrollY;
+    const scroll_height = window.pageYOffset;
+
+    if (scroll_position < 100) {
+        header.classList.remove("scroll");
+    } else {
+        header.classList.add("scroll");
+    }
+})
+
 toggle.addEventListener("click",  () => {
     aside.classList.toggle("active");
     toggle.classList.toggle("active");
     main.classList.toggle("active");
+    header.classList.remove("scroll");
 });
 
 links.forEach(link => {
